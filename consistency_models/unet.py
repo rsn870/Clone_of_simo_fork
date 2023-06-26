d5248e2bfdaaf391b6f76636f3f3ea78690c924c
rsn870 <rsainiranjan@iisc.ac.in>
2023-06-26 14:53:38 +0530

    Update unet.py

--- consistency_models/unet.py
+++ consistency_models/unet.py
@@ -16,12 +16,40 @@ blk = lambda ic, oc: nn.Sequential(
     nn.Conv2d(oc, oc, 3, padding=1),
 )
 
+blk_manage = lambda ic,oc: nn.Sequential(
+    nn.GroupNorm(32, num_channels=ic),
+    nn.ReLU(),
+    nn.Conv2d(ic, oc, 3, padding=1),
+    nn.GroupNorm(32, num_channels=oc),
+    nn.ReLU(),
+    nn.Conv2d(oc, oc, 3, padding=1),
+)
+
+
+def calculate_gram_matrix(features):
+    features = features.view(features.size(0),-1)
+    G = torch.ones((features.shape[0],features.shape[0]), device=features.device)
+    for i in range(features.shape[0]):
+        for j in range(features.shape[0]):
+            G[i,j] = torch.dot(features[i],features[j])/(torch.norm(features[i])*torch.norm(features[j]))
+    return G
+
+
+
+def calculate_vne(Z):
+    rho = torch.matmul(Z.T, Z) / Z.shape[0]
+    eig_val = torch.linalg.eigh(rho)[0][-Z.shape[0]:]
+    return - (eig_val * torch.log(eig_val)).nansum()
+
+def vne_loss(features):
+    G = calculate_gram_matrix(features)
+    vne = calculate_vne(G)
+    vne_coeff = 0.1
+    return -1*vne_coeff * vne
 
 class ConsistencyModel(nn.Module):
     """
     This is ridiculous Unet structure, hey but it works!
-
-    Added intermediate feature functions.
     """
 
     def __init__(self, n_channel: int, eps: float = 0.002, D: int = 128) -> None:
@@ -106,11 +134,13 @@ class ConsistencyModel(nn.Module):
     
     def forward_feature(self, x, t) -> torch.Tensor:
         if isinstance(t, float):
+            t_orig = t
             t = (
                 torch.tensor([t] * x.shape[0], dtype=torch.float32)
                 .to(x.device)
                 .unsqueeze(1)
             )
+
         # time embedding
         args = t.float() * self.freqs[None].to(t.device)
         t_emb = torch.cat([torch.sin(args), torch.cos(args)], dim=-1).to(x.device)
@@ -142,12 +172,186 @@ class ConsistencyModel(nn.Module):
 
         x = self.last(torch.cat([x, x_ori], dim=1))
 
-        t = t - self.eps
+        if t_orig > self.eps :
+            t = t-self.eps
+        
+        
         c_skip_t = 0.25 / (t.pow(2) + 0.25)
         c_out_t = 0.25 * t / ((t + self.eps).pow(2) + 0.25).pow(0.5)
 
         return c_skip_t[:, :, None, None] * x_ori + c_out_t[:, :, None, None] * x,feat
 
+
+    def loss(self, x, z, t1, t2, ema_model):
+        x2 = x + z * t2[:, :, None, None]
+        x2 = self(x2, t2)
+
+        with torch.no_grad():
+            x1 = x + z * t1[:, :, None, None]
+            x1 = ema_model(x1, t1)
+
+        return F.mse_loss(x1, x2)
+    
+    
+    def loss_vne_inc(self, x, z, t1, t2, ema_model):
+        x2 = x + z * t2[:, :, None, None]
+        x2,feat = self.forward_feature(x2, t2)
+
+        with torch.no_grad():
+            x1 = x + z * t1[:, :, None, None]
+            x1 = ema_model(x1, t1)
+
+        return F.mse_loss(x1, x2) + vne_loss(feat)
+
+    def loss_vne_diff(self, x, z, t1, t2, ema_model):
+        x2 = x + z * t2[:, :, None, None]
+        x2,feat = self.forward_feature(x2, t2)
+
+        with torch.no_grad():
+            x1 = x + z * t1[:, :, None, None]
+            x1,feat1 = ema_model.forward_feature(x1, t1)
+
+        feat1=feat1[torch.randperm(feat1.size()[0])]
+
+        feat = feat.view(feat.size()[0],-1)
+        feat1 = feat1.view(feat1.size()[0],-1)
+
+        vne = calculate_vne(torch.abs(feat1-feat))
+
+        return F.mse_loss(x1, x2) + vne
+
+    @torch.no_grad()
+    def sample(self, x, ts: List[float]):
+        x = self(x, ts[0])
+
+        for t in ts[1:]:
+            z = torch.randn_like(x)
+            x = x + math.sqrt(t**2 - self.eps**2) * z
+            x = self(x, t)
+
+        return x
+    @torch.no_grad()
+    def sample_intermediates(self, x, ts: List[float]):
+        lst = []
+        x = self.forward_feature(x, ts[0])
+        lst.append(x)
+
+        for t in ts[1:]:
+            z = torch.randn_like(x)
+            if t**2  > self.eps:
+                x = x + math.sqrt(t**2 - self.eps**2) * z
+            else : 
+                x = x + t * z
+
+
+            x = self(x, t)
+            lst.append(x)
+
+        return x,lst
+    @torch.no_grad()
+    def sample_feature_intermediates(self, x, ts: List[float]):
+        lst = []
+        x,feat = self.forward_feature(x, ts[0])
+        lst.append(feat)
+
+        for t in ts[1:]:
+            z = torch.randn_like(x)
+            if t**2  > self.eps:
+                 x = x + math.sqrt(t**2 - self.eps**2) * z
+            else : 
+                x = x + t * z
+
+
+            x,feat = self.forward_feature(x, t)
+            lst.append(feat)
+
+        return x,lst
+
+
+class ConsistencyModel_Easy(nn.Module):
+    """
+    This is ridiculous Unet structure, hey but it works!
+    """
+
+    def __init__(self, n_channel: int, eps: float = 0.002, D: int = 128) -> None:
+        super(ConsistencyModel_Easy, self).__init__()
+
+        self.eps = eps
+
+        self.freqs = torch.exp(
+            -math.log(10000) * torch.arange(start=0, end=D, dtype=torch.float32) / D
+        )
+
+        self.down = nn.Sequential(
+            *[
+                nn.Conv2d(n_channel, D, 3, padding=1),
+                blk_manage(D, D),
+                blk_manage(D, 2 * D),
+                blk_manage(2 * D, 2 * D),
+            ]
+        )
+
+        self.time_downs = nn.Sequential(
+            nn.Linear(2 * D, D),
+            nn.Linear(2 * D, D),
+            nn.Linear(2 * D, 2 * D),
+            nn.Linear(2 * D, 2 * D),
+        )
+
+        self.mid = blk_manage(2 * D, 2 * D)
+
+        self.up = nn.Sequential(
+            *[
+                blk_manage(2 * D, 2 * D),
+                blk_manage(2 * 2 * D, D),
+                blk_manage(D, D),
+                nn.Conv2d(2 * D, 2 * D, 3, padding=1),
+            ]
+        )
+        self.last = nn.Conv2d(2 * D + n_channel, n_channel, 3, padding=1)
+
+    def forward(self, x, t) -> torch.Tensor:
+        if isinstance(t, float):
+            t = (
+                torch.tensor([t] * x.shape[0], dtype=torch.float32)
+                .to(x.device)
+                .unsqueeze(1)
+            )
+        # time embedding
+        args = t.float() * self.freqs[None].to(t.device)
+        t_emb = torch.cat([torch.sin(args), torch.cos(args)], dim=-1).to(x.device)
+
+        x_ori = x
+
+        # perform F(x, t)
+        hs = []
+        for idx, layer in enumerate(self.down):
+            if idx % 2 == 1:
+                x = layer(x) + x
+            else:
+                x = layer(x)
+                x = F.interpolate(x, scale_factor=0.5)
+                hs.append(x)
+
+            x = x + self.time_downs[idx](t_emb)[:, :, None, None]
+
+        x = self.mid(x)
+
+        for idx, layer in enumerate(self.up):
+            if idx % 2 == 0:
+                x = layer(x) + x
+            else:
+                x = torch.cat([x, hs.pop()], dim=1)
+                x = F.interpolate(x, scale_factor=2, mode="nearest")
+                x = layer(x)
+
+        x = self.last(torch.cat([x, x_ori], dim=1))
+
+        t = t - self.eps
+        c_skip_t = 0.25 / (t.pow(2) + 0.25)
+        c_out_t = 0.25 * t / ((t + self.eps).pow(2) + 0.25).pow(0.5)
+
+        return c_skip_t[:, :, None, None] * x_ori + c_out_t[:, :, None, None] * x
 
     def loss(self, x, z, t1, t2, ema_model):
         x2 = x + z * t2[:, :, None, None]
@@ -172,7 +376,7 @@ class ConsistencyModel(nn.Module):
     @torch.no_grad()
     def sample_intermediates(self, x, ts: List[float]):
         lst = []
-        x = self.forward_feature(x, ts[0])
+        x = self(x, ts[0])
         lst.append(x)
 
         for t in ts[1:]:
@@ -180,18 +384,5 @@ class ConsistencyModel(nn.Module):
             x = x + math.sqrt(t**2 - self.eps**2) * z
             x = self(x, t)
             lst.append(x)
-
-        return x,lst
-    @torch.no_grad()
-    def sample_feature_intermediates(self, x, ts: List[float]):
-        lst = []
-        x,feat = self.forward_feature(x, ts[0])
-        lst.append(feat)
-
-        for t in ts[1:]:
-            z = torch.randn_like(x)
-            x = x + math.sqrt(t**2 - self.eps**2) * z
-            x,feat = self.forward_feature(x, t)
-            lst.append(feat)
 
         return x,lst
